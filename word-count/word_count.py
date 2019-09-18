81d5fd8dfc43e9ca4384512af5493c3cd029e72d
Natalie Yang <natalie.yang@chicagobooth.edu>
2019-09-18 19:33:46 +0000

    word count

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,9 +1,11 @@
 def count_words(sentence):
     sentence = sentence.lower()
     sentence_unique = sentence.split()
+    count_dict = {}
     for word in sentence_unique:
         if(sentence_unique.count(word) > 1):
             sentence_unique.remove(word)
     for word in sentence_unique:
         count = sentence.count(word)
-        print(word + ": " + str(count))
+        count_dict.update({word : count})
+    return(count_dict)
