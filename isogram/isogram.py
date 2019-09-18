929c036dbb4856419e4bb622c70a99c8188951b9
Zizhe Xia <xiazizhejordan@gmail.com>
2019-09-18 16:05:19 +0000

    isogram

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,7 @@
+from collections import Counter
 def is_isogram(string):
-    pass
+    counter = Counter(string)
+    for i in counter:
+        if counter[i] > 1 and i != ' ' and i != '-':
+            return True
+    return False
